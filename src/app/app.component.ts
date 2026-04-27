df48eff059fcaaef81f7d3fb93218e0c52e37013
afrid268 <afridafsal963@gmail.com>
2026-04-27 06:20:58 +0530

    furthe4r refactor of components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
+import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
+import { ServerTrafficStatusComponent } from './dashboard/server-traffic-status/server-traffic-status.component';
+import { SupportTicketsComponent } from './dashboard/support-tickets/support-tickets.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [NavbarComponent, DashboardComponent],
+  imports: [
+    NavbarComponent,
+    DashboardItemComponent,
+    ServerStatusComponent,
+    ServerTrafficStatusComponent,
+    SupportTicketsComponent,
+  ],
 })
 export class AppComponent {}
--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -8,8 +8,5 @@ import { Component, Input } from '@angular/core';
   styleUrl: './server-status.component.css',
 })
 export class ServerStatusComponent {
-  @Input() _currentStatus: string = 'online';
-  get currentStatus() {
-    return this._currentStatus;
-  }
+  currentStatus = 'online';
 }
