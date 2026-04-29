0c4a03895e6b50bc62f8bed608f872749a2e009d
Afrid Afsal <afrid@ionob.com>
2026-04-29 12:05:40 +0530

    content projection,projection as and selector variant

---
+++ src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from "../../../shared/button/button.component";
+import { ControllComponent } from "../../../shared/controll/controll.component";
+
+@Component({
+  selector: 'app-new-ticket',
+  standalone: true,
+  imports: [ButtonComponent, ControllComponent],
+  templateUrl: './new-ticket.component.html',
+  styleUrl: './new-ticket.component.css'
+})
+export class NewTicketComponent {
+
+}
