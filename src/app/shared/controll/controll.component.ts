46850b28b3c4a9181e014083cb8c10cc4363e0ab
Afrid Afsal <afrid@ionob.com>
2026-05-14 11:34:25 +0530

    host listeners and host bindings of property and events new and old methods

--- src/app/shared/controll/controll.component.ts
+++ src/app/shared/controll/controll.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-controll',
@@ -8,10 +8,19 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   styleUrl: './controll.component.css',
   encapsulation: ViewEncapsulation.None,
   host: {
-     class: 'controll'
+     class: 'controll',
+     '(click)' : 'onClick()'//listens to event in text arwea component control
   }
 })
 export class ControllComponent {
+ // @HostBinding('class') className = 'controll'; // looks at the prop name and add the prop to host element and sets the va;ue for the property , preferred not to use this , use host instead
 
+  //@HostListener('click')   onClick() { 
+  //   console.log("test clicked")
+  // }//binds a method to an event 
   label = input.required<string>();
+
+  onClick() { 
+    console.log("test clicked")
+  }
 }
